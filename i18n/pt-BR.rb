53e640f871163266a119f9f74cc442c2e26f2077
under-sama <under@insicuri.net>
2014-10-03 02:16:07 -0300

    pt-br translation

---
+++ i18n/pt-BR.rb
@@ -0,0 +1,77 @@
+{
+  # Misc
+  time_format: '%d/%m/%Y %R',
+  denied: 'Negado.',
+  done: 'Feito.',
+  no_results: 'Nada encontrado.',
+  db_error: 'Erro no banco de dados.',
+  server_error: 'Erro Interno de Servidor',
+  bad_request: 'Má solicitação.',
+  not_implemented: 'Não implementado.',
+  
+  # Posting validation
+  name_too_long: 'O nome é longo demais.',
+  title_too_long: 'O título é longo demais.',
+  comment_too_long: 'O comentário é longo demais.',
+  comment_empty: 'O comentário não pode estar vazio.',
+  title_empty: 'O título não pode estar vazio.',
+  invalid_chars: 'Seu post contém caracteres inváldos.',
+  fast_post: 'Você precisa esperar por um momento antes de fazer outra postagem.',
+  honeypot: 'Não preencha este campo.',
+  thread_full: 'Limite de respostas alcançado.',
+  cmd_forbidden: 'Você não pode usar este comando.',
+  
+  # File validation
+  file_size_too_big: 'Tamanho de arquivo muito grande.',
+  dimensions_too_large: 'Dimensões de imagem muito largas.',
+  dup_file_thread: 'Um tópico com o mesmo arquivo já existe.',
+  dup_file_reply: 'Este arquivo já foi postado neste tópico.',
+  bad_file_format: 'Formato de arquivo inválido.',
+  duration_too_long: 'Duração longa demais.',
+  webm_audio_disabled: 'Fluxos de áudio não são permitidos.',
+  invalid_audio: 'Fluxo de áudio inválido.',
+  invalid_video: 'Fluxo de vídeo inválido.',
+  invalid_stream: 'Fluxo inválido.',
+  no_video_streams: 'O arquivo não tem nenhum fluxo de vídeo válido.',
+  too_many_video: 'Somente um fluxo de vídeo é permitido',
+  too_many_audio: 'Somente um fluxo de áudio é permitido.',
+  
+  # Path validation
+  bad_board: 'Board inválida.',
+  bad_thread: 'Tópico inválido.',
+  bad_post: 'Postagem inválida.',
+  
+  # Admin
+  nope: "Não posso deixar você fazer isso.",
+  fast_auth: 'Você precisa esperar um momento antes de tentar logar novamente.',
+  confirm_keyword: 'SIM',
+  bad_confirm_keyword: 'Palavra-chave de confirmação errada.',
+  bad_slug: 'Tentativa inválida.',
+  bad_title: 'Título inválido.',
+  bad_config_json: 'Configuração inválida.',
+  
+  passwd_too_short: 'A senha muito curta (o mínimo é de 8 letras).',
+  passwd_mismatch: 'A senha não combina com a confirmação.',
+  
+  invalid_ip: 'Endereço de IP inválido',
+  invalid_ban_id: 'ID de ban inválido',
+  cannot_edit_expired_ban: 'Você não pode editar bans expirados',
+  empty_ban_reason: 'O motivo do ban não pode estar vazio.',
+  
+  not_banned: 'Você não está banido.',
+  
+  bad_user_id: 'Não foi possível encontrar o usuário.',
+  bad_user_name: 'Nome de usuário inválido.',
+  bad_user_level: 'Nível de usuário inválido.',
+  new_passwd: 'A senha gerada é %s',
+  user_levels: {
+    admin: 'Administrador',
+    mod: 'Moderador'
+  },
+  
+  # Capcodes
+  user_trips: {
+    'admin' => 'Admin',
+    'mod' => 'Mod'
+  }
+}
