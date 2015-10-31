a1c962bee58399ee34fb61602e56924f80775097
Desuwa <desuwane@gmail.com>
2015-10-31 04:42:10 +0100

    Version 0.5.3

--- hive.rb
+++ hive.rb
@@ -22,7 +22,7 @@ Encoding.default_external = 'UTF-8'
 module Hive
 
 class BBS < Sinatra::Base
-  VERSION = '0.5.2'
+  VERSION = '0.5.3'
   
   Dir.glob("#{settings.root}/helpers/*.rb").each { |f| require f }
   
